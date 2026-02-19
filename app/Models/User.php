a46fb1676101aefc4831e86ceb017edc4961e3cf
cesarapm <garyfeomm@gmail.com>
2026-02-19 14:06:19 -0600

    admin entro

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Filament\Panel;
 
 class User extends Authenticatable
 {
@@ -49,8 +50,13 @@ class User extends Authenticatable
         ];
     }
 
-    public function canAccessPanel(): bool
-    {
-        return $this->role === 'admin';
-    }
+    // public function canAccessPanel(): bool
+    // {
+    //     return $this->role === 'admin';
+    // }
+
+    public function canAccessPanel(Panel $panel): bool
+{
+    return $this->role === 'admin' && $this->activo == 1;
+}
 }
